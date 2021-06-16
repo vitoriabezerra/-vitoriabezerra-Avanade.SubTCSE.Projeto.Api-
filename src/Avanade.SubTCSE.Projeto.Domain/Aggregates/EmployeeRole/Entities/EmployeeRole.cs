adeb17969180b88942d4263f8df6e9fc29dd7208
Vitória Bezerra <vitoria0406@gmail.com>
2021-06-16 10:29:47 -0300

    adding folders and new class EmployeeRole

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,19 @@
+﻿namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    public record EmployeeRole
+    {
+
+
+
+        public EmployeeRole(string id, string rolename)
+        {
+            Id = id;
+            RoleName = rolename;
+        }
+        public string Id { get; set; }
+
+
+
+        public string RoleName { get; init; }
+    }
+}
